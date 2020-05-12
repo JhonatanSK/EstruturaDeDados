7fd8384939c0ccdb1e213f7ee4a2c4f2f02062c6
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-12 20:51:43 -0300

    feature: desenvolvendo a view da lista singularmente encadeada Exercicio 24

---
+++ EstruturaDeDados/estruturaDeDadosSegundaEntrega/estruturaDeDadosEx24/ListaSE_View.java
@@ -0,0 +1,29 @@
+package estruturaDeDadosEx24;
+
+import javax.swing.*;	
+
+public class ListaSE_View {	
+	private	String[]  sSN = {"Sim", "Não"};
+	private	String 	sIO = "Selecione a opção desejada:";
+	private	String	sExit = "Deseja sair?";
+
+	public void	mostra(String sT, String sI) {
+		JOptionPane.showMessageDialog(null, sI, sT,JOptionPane.PLAIN_MESSAGE);		
+	}
+	
+	public String recebe(String sT, String sI) {
+		return JOptionPane.showInputDialog(null,sI,sT,JOptionPane.QUESTION_MESSAGE);
+	}
+	
+	public int menu(String sT, String[] sOpc) {
+		int iOpcao, iSN;
+		do		{
+			iOpcao = JOptionPane.showOptionDialog(	null,sIO,sT,1,JOptionPane.QUESTION_MESSAGE,null, sOpc,null	);
+			if(iOpcao == sOpc.length-1) {
+				iSN = JOptionPane.showOptionDialog(	null,sExit,sT,1, JOptionPane.QUESTION_MESSAGE,null,sSN,null	);
+				if(iSN == 1)	iOpcao = -1;
+			}
+		}while(iOpcao < 0 || iOpcao > sOpc.length-1);
+		return iOpcao;
+	}
+}
