382f5abd175f182c77fc8d1dfe1309ee1cb53e0b
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-26 19:19:44 -0300

    feature: desenvolvendo o nó da arvore binária e a estrutura arvore binária com o metodo de apresentar seus valores pré-ordenados

---
+++ EstruturaDeDados/estruturaDeDadosQuartaEntrega/estruturaDeDadosEx31/Node.java
@@ -0,0 +1,39 @@
+package estruturaDeDadosEx31;
+
+public class Node {
+	private	int	element;
+	private	Node left;
+	private	Node right;
+
+	public	Node()	{
+		element = 0;
+		left = null;
+		right = null;
+	}
+	
+	public	Node(int element, Node left, Node right)	{
+		this.element = element;
+		this.left = left;
+		this.right = right;
+	}
+	
+	void setLeft(Node	prev)	{
+		this.left = prev;
+	}
+
+	void setRight(Node	right)	{
+		this.right = right;
+	}
+	
+	int	getElement()	{
+		return	element;
+	}
+	
+	Node getLeft() {
+		return	left;
+	}
+
+	Node getRight()	{
+		return	right;
+	}
+}
